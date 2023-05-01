054b49ae84ffb89355746e39f9ea9223b9cf9350
davda54 <david.samuel@seznam.cz>
2023-05-02 01:58:23 +0200

    NoCoLA training/evaluation

---
+++ nocola/dataset_t5.py
@@ -0,0 +1,63 @@
+from smart_open import open
+
+import torch
+from torch.utils.data import Dataset
+
+
+class ColaDataset(Dataset):
+    def __init__(self, path: str):
+        self.sentences = []
+        self.labels = []
+
+        is_correct = False
+        n_correct, n_incorrect = 0, 0
+        for line in open(path):
+            line = line.strip()
+            if len(line) == 0 or line == "Ungrammatical:":
+                continue
+            if line == "Grammatical:":
+                is_correct = True
+                continue
+
+            if is_correct: n_correct += 1
+            else: n_incorrect += 1
+
+            line = line.split('\t')[0]
+
+            self.sentences.append(line)
+            self.labels.append("riktig" if is_correct else "uriktig")
+
+        print(n_correct, n_incorrect, flush=True)
+
+    def __getitem__(self, index):
+        return self.sentences[index], self.labels[index]
+
+    def __len__(self):
+        return len(self.sentences)
+
+
+class CollateFunctor:
+    def __init__(self, tokenizer, max_length): 
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __call__(self, samples):
+        sentences = [sentence for sentence, _ in samples]
+        labels = [label for _, label in samples]
+
+        source_encoding = self.tokenizer(
+            sentences,
+            max_length=self.max_length,
+            return_attention_mask=True,
+            return_tensors='pt',
+            padding=True
+        )
+        target_encoding = self.tokenizer(
+            labels,
+            max_length=self.max_length,
+            return_attention_mask=True,
+            return_tensors='pt',
+            padding=True
+        )
+
+        return source_encoding.input_ids, source_encoding.attention_mask, target_encoding.input_ids
